a3c31382532d8097b5ae9a05ac19cf607026a547
sanjay <san638233@gmail.com>
2025-05-28 18:05:02 +0530

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -24,7 +24,7 @@ function Contact() {
       initial={{opacity:0,y:30}}
       whileInView={{opacity:1,y:0}}
       transition={{duration:0.7,delay:0.7}}>
-        //yes
+        
 
   <label>
     Your email:
