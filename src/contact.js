87b530437dc6f24c411f727d77a709f06feba6aa
sanjay <san638233@gmail.com>
2025-05-28 18:01:08 +0530

    Update contact.js

--- src/contact.js
+++ src/contact.js
@@ -7,7 +7,7 @@ function Contact() {
     navigator.clipboard.writeText(text)
     alert('text copy!'+text)
   }
-          //yes
+          
   return (
   
     <>
@@ -51,7 +51,7 @@ function Contact() {
 
 
       <label htmlFor="Ph number">Our ph number </label>
-      <input type="tle" defaultValue={'987654321'} readOnly    onClick={()=>copytext('987654321')}  /><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
+      <input type="tle" defaultValue={'+91 70108 06421'} readOnly    onClick={()=>copytext('987654321')}  /><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
       <label htmlFor="Ph number">Our Email</label>
       <input type="email" defaultValue={'cmsigns@gmail.com '} readOnly   onClick={()=>copytext('cmsigns@gmail.com')}  /><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
       <label htmlFor="socialmedia">Follow us on </label>
